23a1d7bd93c397072a7b970c41b44d618e7b7044
“Zahed75” <zahedhasan.cs@gmail.com>
2023-10-21 20:35:44 +0600

    modified:   lib/RestAPI/RestClient.dart

--- lib/RestAPI/RestClient.dart
+++ lib/RestAPI/RestClient.dart
@@ -3,23 +3,39 @@ import 'package:app_crud/Style/style.dart';
 import 'package:http/http.dart' as http;
 //create Product API Calling
 
-Future<bool> ProductCreateRequest(FormValues)async{
+Future<bool> ProductCreateRequest(FormValues) async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/CreateProduct");
+  var PostBody = json.encode(FormValues);
+  var PostHeader = {"Content-Type": "application/json"};
 
-  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/CreateProduct");
-  var PostBody=json.encode(FormValues);
-  var PostHeader={"Content-Type": "application/json"};
-  
- var response= await http.post(URL, headers:PostHeader,body:PostBody);
- var ResultCode=response.statusCode;
- var ResultBody=json.decode(response.body);
+  var response = await http.post(URL, headers: PostHeader, body: PostBody);
+  var ResultCode = response.statusCode;
+  var ResultBody = json.decode(response.body);
 
- if(ResultCode==200 && ResultBody['status']=="success"){
-  SuccessToast("Request Success~");
-  return true;
- }
- else{
-  ErrorToast("Request Fail Try Again!");
-  return false;
- }
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
+    SuccessToast("Request Success~");
+    return true;
+  } else {
+    ErrorToast("Request Fail Try Again!");
+    return false;
+  }
+}
 
+//GET Proudct
+
+Future <List>ProductGridViewListRequest() async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/ReadProduct");
+  var PostHeader = {"Content-Type": "application/json"};
+
+  var response = await http.get(URL, headers: PostHeader);
+  var ResultCode = response.statusCode;
+  var ResultBody = json.decode(response.body);
+
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
+    SuccessToast("Request Success~");
+    return ResultBody['data'];
+  } else {
+    ErrorToast("Request Fail Try Again!");
+    return [];
+  }
 }
